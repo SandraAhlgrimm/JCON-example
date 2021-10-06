329b63dbeb1a2d5a4a06230dcd5d6fc0675de703
Sandra Ahlgrimm <sandra.kriemann@gmail.com>
2021-10-06 17:57:09 +0000

    init spring project for azure deployment

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * HelloController
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping(value="/")
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+    
+}
